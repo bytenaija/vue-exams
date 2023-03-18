0d891d5e13a1a1621fa8c73d99f63d4315bc8190
Everistus Olumese <everistusolumese@gmail.com>
2023-03-18 13:47:11 -0400

    Vue Exam

---
+++ src/components/NotFound.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <p class="not-found" v-show="!repo">That page cannot be found</p>
+    <p class="not-found" v-show="repo">The repo cannot be found</p>
+
+    <router-link to="/">Home Page</router-link>
+  </div>
+</template>
+
+<script>
+import {RouterLink} from 'vue-router'
+export default {
+name: 'NotFound',
+props: ['repo'],
+components:{
+    RouterLink
+}
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 50%;
+    height: 40vh;
+    margin: 0 auto;
+    padding: 1rem;
+    text-align: center;
+}
+
+.not-found{
+    color: rebeccapurple
+}
+
+</style>
